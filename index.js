e1a7f4f0cc1679681a7e32d97329cc347345f9ce
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-07-02 14:26:49 -0400

    Render users and use wildcard route

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 var express = require('express')
+var fs = require('fs')
+var _ = require('lodash')
 
 // create an instance of app
 var app = express()
 
+// read some dummy data
+let users = []
+fs.readFile('users.json', { encoding: 'utf8' }, function(err, data) {
+  if (err) throw err
+
+  users = JSON.parse(data).map(function(user) {
+    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+    return user
+  })
+  console.log(users)
+})
+
 // define a route, which console logs all requests, then sends it off to
 // other route handlers
 app.get('/:anything', function(req, res, next) {
-  console.log(req)
+  console.log('GET ' + req.url)
   next()
 })
 
 app.get('/', function(req, res, next) {
-  res.send('Hello World!')
+
+  var buffer = users.reduce(function(acc, user) {
+    acc += '<a href="/' + user.username + '">' + user.name.full + '</a></br>'
+    return acc
+  }, '')
+  res.send(buffer)
 })
 
-app.get('/yo', function(req, res) {
-  res.send('Yo!')
+app.get('/:username', function(req, res) {
+  var username = req.params.username
+  res.send(username)
 })
 
 // start the server on port 3000
 var server = app.listen(3000, function() {
   console.log('Server running at http://localhost:' + server.address().port)
 })
-

09b2ab414cf16a75ccc874ce0d8d0378b08f8683
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-07-02 13:46:35 -0400

    Use 'next' in route handler

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ var express = require('express')
 // create an instance of app
 var app = express()
 
-// define a route
-app.get('/', function(req, res) {
-  res.send('Hello, world!')
+// define a route, which console logs all requests, then sends it off to
+// other route handlers
+app.get('/:anything', function(req, res, next) {
+  console.log(req)
+  next()
+})
+
+app.get('/', function(req, res, next) {
+  res.send('Hello World!')
+})
+
+app.get('/yo', function(req, res) {
+  res.send('Yo!')
 })
 
 // start the server on port 3000
